71836fcc6a27f75bc1b381905ba18e2e58a3f2b2
AzzaWafiqurrohmah <wafiqurrohmahazza@gmail.com>
2025-02-12 11:50:41 +0700

    chore: passport config

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
-class User extends Model
+class User extends Authenticatable
 {
+    use HasApiTokens;
+
     public $timestamps = false;
     protected $fillable = [
         'email', 'password'
